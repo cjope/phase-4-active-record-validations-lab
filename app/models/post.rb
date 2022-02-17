832a9f5d84c19204641d041b4d4994d38e4af932
Craig <craigjope@gmail.com>
2022-02-17 10:30:44 -0700

    commit tree 494ea596225d227206bdc73d75842fda3712dbd3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true, uniqueness: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in:["Fiction","Non-Fiction"]}
+    # validates :title, exception: {in:["True Facts"]}
+    # validates :junk
+
+
+    UNWANTED = [
+        "Won't Believe",
+        "Secret",
+        "Top [number]",
+        "Guess"
+]
+
+
+    def junk
+        if self.title.include? [UNWANTED]
+          self.errors.add :name, "I'm not clicking that!"
+        end
+    end
+
 end
